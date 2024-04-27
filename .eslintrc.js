1cca7f2c6b6120ca4e8008a664d3186f3b6b9172
matmirowski <mateusz.m@niceguys.info>
2024-04-27 20:52:04 +0200

    [3] Add linting libraries with initial configuration and apply fixed linting

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+	parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+	extends: [
+		'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+		'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+	],
+	parserOptions: {
+		ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+		sourceType: 'module', // Allows for the use of imports
+	},
+	plugins: ['prettier'],
+	rules: {
+		'no-duplicate-imports': ['error', { includeExports: true }],
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				semi: true,
+			},
+		],
+	},
+	overrides: [
+		{
+			files: ['test/**'],
+			rules: {
+				'@typescript-eslint/no-explicit-any': 'off',
+			},
+		},
+	],
+};
