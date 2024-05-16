e1e1f89ff4307689f3ec28bbd281452fe4a5b80a
matmirowski <mateusz.m@niceguys.info>
2024-05-16 13:35:43 +0200

    [15] Add endpoint for listing branches for admin

--- src/http-api/branch-api/branch-admin.controller.ts
+++ src/http-api/branch-api/branch-admin.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { BranchService } from '../../branch/branch.service';
+import { RequiredAuth } from '../auth/auth-decorator';
+import { UserRole } from '../../user/user';
+import { ListBranchesResponseDto } from './dto/list-branches-response.dto';
+import { ListBranchesQueryDto } from './dto/list-branches-query.dto';
 
 @Controller('admin/branches')
-export class BranchAdminController {}
+export class BranchAdminController {
+	constructor(@Inject(BranchService) private readonly branchService: BranchService) {}
+
+	@RequiredAuth({
+		roles: [UserRole.Admin],
+	})
+	@Get()
+	async list(@Query() query: ListBranchesQueryDto) {
+		const branches = await this.branchService.findBranches({
+			status: query.status,
+		});
+		return ListBranchesResponseDto.fromReadModels(branches);
+	}
+}
