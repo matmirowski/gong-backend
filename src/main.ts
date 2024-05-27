949b72c6afbd7e062a84efbd2968e0fcb81c76f2
Szymon Tarnowski <szymontarno@gmail.com>
2024-05-27 16:21:56 +0200

    [26] Handle image upload

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DbClient } from './database/db-client';
 import { ValidationPipe } from '@nestjs/common';
+import { json } from 'body-parser';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, {
+		bodyParser: false,
+	});
 
 	app.useGlobalPipes(new ValidationPipe());
 
@@ -12,6 +15,8 @@ async function bootstrap() {
 	await dbClient.applyDbSchema();
 
 	app.enableCors();
+
+	app.use(json({ limit: '50mb' }));
 
 	await app.listen(3030);
 }
